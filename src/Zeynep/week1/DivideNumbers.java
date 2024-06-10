bc6ad27665c5f28a38d1ca7a6af86ea8b321b2c8
zeynepsh7 <avzeynepsoysal@hotmail.com>
2024-06-09 19:07:37 -0700

    Zeynep week 1 updates

--- src/Zeynep/week1/DivideNumbers.java
+++ src/Zeynep/week1/DivideNumbers.java
@@ -5,7 +5,6 @@ public class DivideNumbers {
 
         divideNumbers(26, 5);
     }
-
     public static void divideNumbers(int num1, int num2) {
 
         if (num2 == 0) {
@@ -20,7 +19,6 @@ public class DivideNumbers {
             count++;
             num1 -= num2;
         }
-
         System.out.println(result + count + " with remainder " + num1);
     }
 }
