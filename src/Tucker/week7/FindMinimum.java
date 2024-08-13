3651b5b156cfb0a21b9993c2bfecb75bd2fc978c
tuckstpltn <tuckerstapleton@gmail.com>
2024-08-12 21:10:47 -0600

    Finished FindMinimum

--- src/Tucker/week7/FindMinimum.java
+++ src/Tucker/week7/FindMinimum.java
@@ -3,10 +3,26 @@ package Tucker.week7;
 public class FindMinimum {
 
     public static void main(String[] args){
+        int[] arr = {99, 12, 23, 32, 44, 57, 6};
+        int[] arr2 = {254, 23, 9999, 1, 43, 72};
 
+        System.out.println(findMin(arr));
+        System.out.println(findMin(arr2));
 
 
     }
+
+    public static int findMin(int[] array){
+
+        int min = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+return min;
+    }
+
 }
 
 /*
