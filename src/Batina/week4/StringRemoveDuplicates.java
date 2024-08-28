3c7f80e9da0582844389163b5c79bf2f19174d83
batina7777 <batina.adams@gmail.com>
2024-08-28 09:36:12 -0600

    ConcatArray

---
+++ src/Batina/week4/StringRemoveDuplicates.java
@@ -0,0 +1,8 @@
+package Batina.week4;
+
+public class StringRemoveDuplicates {
+//    Write a return method that can remove the duplicated values from the String
+//    Ex: removeDup("AAABBBCCC") ==> ABC
+
+
+}
---
+++ src/Batina/week4/StringSameLetters.java
@@ -0,0 +1,10 @@
+package Batina.week4;
+
+public class StringSameLetters {
+
+//    Write a return method that checks if a string is built out of the same letters as another string.
+//    Ex: same("abc", "cab"); ==> true
+//    same("abc", "abb"); ==> false
+
+
+}
